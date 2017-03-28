e0b49594cfba19f6e21c4acf5251a5984185ea2d
Puyih Severino <yihseverino@gmail.com>
2017-03-28 09:07:50 +0800

    redirect  form to self

--- suggest.php
+++ suggest.php
@@ -1,4 +1,25 @@
-<?php 
+<?php
+
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+    var_dump($_POST);
+    var_dump($_GET);
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $details = $_POST['details'];
+
+    echo "<pre>";
+    $html_body = "";
+    $html_body .= "NAME: " .  $name . "\n";
+    $html_body .= "EMAIL: " . $email . "\n";    
+    $html_body .= "DETAILS: " . $details . "\n";
+
+    echo $html_body;
+    echo "</pre>";
+
+    header("location:suggest.php?status=thanks");
+}
+
 $pageTitle = "Suggest A Media Item";
 $section = "suggest";
 
@@ -7,9 +28,13 @@ include('inc/header.php'); ?>
 <div class="section page">
     <div class="wrapper">
         <h1>Suggest a media item</h1>
+        <?php
+        if(isset($_GET['status']) && $_GET['status'] == "thanks") {
+            echo "<p>Thanks for the email I&rsquo;ll checkout your suggestions shortly!</p>";
+        } else { ?>
         <p>Let me know  your suggestions. Complete the form to send me an email</p>
     </div>
-    <form method="post" action="process.php">
+    <form method="post" action="suggest.php">
         <table>
             <tr>
                 <th><label for="name">Name:</label></th>
@@ -24,9 +49,8 @@ include('inc/header.php'); ?>
                 <td><textarea id="details" name="details">default</textarea></td>
             </tr>
         </table>
-        <input type="submit" value="send"/>
-        
-        
+        <input type="submit" value="send"/>        
     </form>
+      <?php } ?>
 </div>
 <?php include('inc/footer.php'); ?>
